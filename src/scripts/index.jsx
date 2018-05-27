af30bbd27998a8af2443312cf2568d2798e21eff
Maximo Belen <maximo.fassnacht@exozet.com>
2018-05-27 22:28:10 +0200

    page

--- src/scripts/index.jsx
+++
@@ -1,9 +0,0 @@
-import 'core-js/fn/object/assign';
-import "./../styles/main.scss";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PacmanApp from './pacmanApp.jsx';
-
-const rootEl = document.getElementById('app-container');
-
-ReactDOM.render(<PacmanApp/>, rootEl);
